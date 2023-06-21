74d8dcf65c9ae47af439bea4be8befc3b5a55ae5
Josh Hunt <30326121+JHunt16@users.noreply.github.com>
2023-06-21 13:46:44 +0100

    Add getters and setters to each model so the data will be available in the objects when the request is received.

--- sqs-tester/src/main/java/uk/gov/justice/laa/crime/microservice/sqstester/model/MetaData.java
+++ sqs-tester/src/main/java/uk/gov/justice/laa/crime/microservice/sqstester/model/MetaData.java
@@ -2,4 +2,12 @@ package uk.gov.justice.laa.crime.microservice.sqstester.model;
 
 public class MetaData {
     private String laaTransactionId;
+
+    public String getLaaTransactionId() {
+        return laaTransactionId;
+    }
+
+    public void setLaaTransactionId(String laaTransactionId) {
+        this.laaTransactionId = laaTransactionId;
+    }
 }
--- sqs-tester/src/main/java/uk/gov/justice/laa/crime/microservice/sqstester/model/Request.java
+++ sqs-tester/src/main/java/uk/gov/justice/laa/crime/microservice/sqstester/model/Request.java
@@ -14,4 +14,92 @@ public class Request {
     private Defendant defendant;
     private List<Session> sessions;
     private MetaData metadata;
+
+    public String getMaatId() {
+        return maatId;
+    }
+
+    public void setMaatId(String maatId) {
+        this.maatId = maatId;
+    }
+
+    public String getCaseUrn() {
+        return caseUrn;
+    }
+
+    public void setCaseUrn(String caseUrn) {
+        this.caseUrn = caseUrn;
+    }
+
+    public String getAsn() {
+        return asn;
+    }
+
+    public void setAsn(String asn) {
+        this.asn = asn;
+    }
+
+    public String getCjsAreaCode() {
+        return cjsAreaCode;
+    }
+
+    public void setCjsAreaCode(String cjsAreaCode) {
+        this.cjsAreaCode = cjsAreaCode;
+    }
+
+    public String getCreatedUser() {
+        return createdUser;
+    }
+
+    public void setCreatedUser(String createdUser) {
+        this.createdUser = createdUser;
+    }
+
+    public String getCjsLocation() {
+        return cjsLocation;
+    }
+
+    public void setCjsLocation(String cjsLocation) {
+        this.cjsLocation = cjsLocation;
+    }
+
+    public String getDocLanguage() {
+        return docLanguage;
+    }
+
+    public void setDocLanguage(String docLanguage) {
+        this.docLanguage = docLanguage;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+
+    public Defendant getDefendant() {
+        return defendant;
+    }
+
+    public void setDefendant(Defendant defendant) {
+        this.defendant = defendant;
+    }
+
+    public List<Session> getSessions() {
+        return sessions;
+    }
+
+    public void setSessions(List<Session> sessions) {
+        this.sessions = sessions;
+    }
+
+    public MetaData getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(MetaData metadata) {
+        this.metadata = metadata;
+    }
 }
