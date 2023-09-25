1a4ecc14b467a3d51e5b432c957c786f6cddc19f
Josh Hunt <30326121+JHunt16@users.noreply.github.com>
2023-09-25 10:18:32 +0100

    Remove SQS connection code that uses credentials to (hopefully) allow the application to use short-lived credentials.

--- sqs-tester/src/main/java/uk/gov/justice/laa/crime/microservice/sqstester/message_queue/MessagingConfig.java
+++ sqs-tester/src/main/java/uk/gov/justice/laa/crime/microservice/sqstester/message_queue/MessagingConfig.java
@@ -7,29 +7,17 @@ import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 import io.awspring.cloud.messaging.core.QueueMessagingTemplate;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor
 @Configuration
+@ConditionalOnProperty(value = "cloud.aws.sqs.enabled", havingValue = "true")
 public class MessagingConfig {
-    private final SqsProperties sqsProperties;
 
     @Bean
     public QueueMessagingTemplate queueMessagingTemplate(AmazonSQSAsync amazonSQS) {
         return new QueueMessagingTemplate(amazonSQS);
-    }
-
-    @Bean
-    public AmazonSQSAsync amazonSQSAsync() {
-        return AmazonSQSAsyncClientBuilder
-                .standard()
-                .withRegion(Regions.fromName(sqsProperties.getRegion()))
-                .withCredentials(
-                        new AWSStaticCredentialsProvider(
-                                new BasicAWSCredentials(sqsProperties.getAccesskey(), sqsProperties.getSecretkey())
-                        )
-                )
-                .build();
     }
 }
