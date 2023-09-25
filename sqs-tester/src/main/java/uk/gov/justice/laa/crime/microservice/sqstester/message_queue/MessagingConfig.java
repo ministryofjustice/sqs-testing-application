df3bb63f37c1ab1d10539ada0765d0bbb2e724ef
Josh Hunt <30326121+JHunt16@users.noreply.github.com>
2023-09-25 10:30:58 +0100

    Restore some removed code and amend the AmazonSQSAsyncClientBuilder to not take key and secret as the last commit didn't work.

--- sqs-tester/src/main/java/uk/gov/justice/laa/crime/microservice/sqstester/message_queue/MessagingConfig.java
+++ sqs-tester/src/main/java/uk/gov/justice/laa/crime/microservice/sqstester/message_queue/MessagingConfig.java
@@ -1,7 +1,5 @@
 package uk.gov.justice.laa.crime.microservice.sqstester.message_queue;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
@@ -16,8 +14,17 @@ import org.springframework.context.annotation.Configuration;
 @ConditionalOnProperty(value = "cloud.aws.sqs.enabled", havingValue = "true")
 public class MessagingConfig {
 
+    private final SqsProperties sqsProperties;
     @Bean
     public QueueMessagingTemplate queueMessagingTemplate(AmazonSQSAsync amazonSQS) {
         return new QueueMessagingTemplate(amazonSQS);
+    }
+
+    @Bean
+    public AmazonSQSAsync amazonSQSAsync() {
+        return AmazonSQSAsyncClientBuilder
+                .standard()
+                .withRegion(Regions.fromName(sqsProperties.getRegion()))
+                .build();
     }
 }
---
+++ sqs-tester/src/main/java/uk/gov/justice/laa/crime/microservice/sqstester/message_queue/SqsProperties.java
@@ -0,0 +1,13 @@
+package uk.gov.justice.laa.crime.microservice.sqstester.message_queue;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@ConfigurationProperties(prefix = "cloud-platform.aws.sqs")
+@Component
+public class SqsProperties {
+
+    private String region;
+}
